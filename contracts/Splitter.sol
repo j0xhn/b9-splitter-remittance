3b7e1cfddd5e1db4356772880691960fcde7fdea
John D. Storey <johndangerstorey@gmail.com>
2017-09-02 12:56:17 +0200

    finishes basic splitter contract

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,58 +1,50 @@
 pragma solidity ^0.4.6;
 
+import './MappingWithStruct.sol';
+
 // NOTES:
 
 // This implemenation is for multi use
-// each funder supplies an ID & approved address. 
-// the person to withdraw submits the correct password/id pair
-// splits off a .01 eth fee to the owner
+// each funder supplies the approved address to withdraw. 
+// on withdraw contract splits off a .01 eth fee to the owner
+
+// todo:    Create a deadline in blocks, assign that to the entity
+//          in order to allow owner to withdraw after deadline expires.
 
 
-contract Splitter {
+contract Splitter is MappingWithStruct {
     address public owner;
     uint public balance;
+    uint public fee = 10000000000000000; // .01 Eth
 
     function Splitter() { owner = msg.sender; }
 
-    struct EntityStruct {
-        address entityAddress;
-        uint amount;
-    }
-
-    EntityStruct[] public entityStructs;
-    mapping(address => bool) knownEntity;
-
-    function isEntity(address entityAddress) public constant returns(bool isIndeed) {
-        return knownEntity[entityAddress];
-    }
-
-    function getEntityCount() public constant returns(uint entityCount) {
-        return entityStructs.length;
-    }
-
-    function newEntity(address entityAddress, uint _amount) public returns(uint rowNumber) {
-        if (isEntity(entityAddress)) {revert();}
-        EntityStruct storage instance;
-        instance.amount = _amount;
-        knownEntity[entityAddress] = true;
-        return entityStructs.push(instance) - 1;
-    }
-
-
-    function Withdraw() {
+    function Withdraw() returns(bool) {
         // make sure it's actually been assigned a value
-        if (isEntity(entityStructs[msg.sender])) {revert();}
+        if (!isEntity(msg.sender)) {revert();}
 
+        // a little for me, the rest for you
         var instance = entityStructs[msg.sender];
-        msg.sender.transfer(instance.amount);
+        owner.transfer(fee);
+        msg.sender.transfer(instance.amount - fee);
+        // resolve as complete
+        deleteEntity(msg.sender);
         return true;
     }
 
-    function Fund(address _approvedAddress) 
+    function Fund(address approvedAddress) 
         payable
+        returns(bool)
     {
-        // Appropriates how much can be withdrawn
-        entityStructs[_approvedAddress].amount += msg.value;
+        // Checks to make sure minimum fee is met
+        if (msg.value <= fee) {revert();}
+
+        // Increases amount, or creates amount
+        if (isEntity(approvedAddress)) {
+            entityStructs[approvedAddress].amount += msg.value;
+        } else {
+            newEntity(approvedAddress, msg.value);
+        }
         return true;
     }
 
